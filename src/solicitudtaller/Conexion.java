8ed56fd0431c629cb2e4f1f57be921b5cf658931
balem <enriquev.rodriguez@gmail.com>
2012-07-30 08:59:17 -0400

    Niveles de usuario y vistas de solicitudes

---
+++ src/solicitudtaller/Conexion.java
@@ -0,0 +1,86 @@
+package solicitudtaller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Conexion {
+    Connection con;
+    Statement snt;
+    ResultSet rs;
+    static String server = "localhost";
+    static String usr = "root";
+    static String pass = "infinito";
+    static String db = "solicitud2";
+    String url = "jdbc:mysql://"+server+"/"+db;
+    
+    Conexion(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection(url,usr, pass);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public String dateFormat(Date ddate){
+            java.text.SimpleDateFormat dia = new java.text.SimpleDateFormat("yyyy-MM-dd");
+            String Fecha = dia.format(ddate);
+            return Fecha;
+        }
+    
+    public String gencod(String sql){
+             int cod = 0;
+        try {
+            snt = con.createStatement();
+            rs = snt.executeQuery(sql);
+            rs.next() ;
+            cod = rs.getInt("newcod");
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            return String.valueOf(cod);
+   }
+    
+     public int buscaCod(String sql){
+                int aux = 0;
+        try {
+            snt = con.createStatement();
+            rs = snt.executeQuery(sql);
+            if (rs.next()) {
+                aux = rs.getInt("id");
+            }
+        } catch (SQLException ex) {
+               Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+                 return aux;
+     }
+     
+    public int obtenerRol(String usuario){
+        int aux = 0;
+        try {
+            String sql = "SELECT rol FROM usuario where username like '%"+usuario+"%'";
+            snt = con.createStatement();
+            rs = snt.executeQuery(sql);
+            rs.next();
+            aux = rs.getInt("rol");
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.print(aux);
+        return aux;
+        
+    }
+    
+    
+}
---
+++ src/solicitudtaller/HashMd5.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solicitudtaller;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author Daniel San Nicolas
+ */
+public class HashMd5 {
+
+   private static final char[] HEXADECIMAL = { '0', '1', '2', '3',
+        '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
+    public  String hash(String stringToHash)  {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] bytes = md.digest(stringToHash.getBytes());
+            StringBuilder sb = new StringBuilder(2 * bytes.length);
+            for (int i = 0; i < bytes.length; i++) {
+                int low = (int)(bytes[i] & 0x0f);
+                int high = (int)((bytes[i] & 0xf0) >> 4);
+                sb.append(HEXADECIMAL[high]);
+                sb.append(HEXADECIMAL[low]);
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            //exception handling goes here
+            return null;
+        }
+    }
+}
